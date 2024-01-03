92d2122a9f5b61fef951d6e5a146ae02752580de
Silas Kraume <silas.kraume1552@gmail.com>
2024-01-03 22:21:26 +0100

    fix test to be project conform

--- Testcases/pos/test18.jl
+++ Testcases/pos/test18.jl
@@ -1,25 +1,18 @@
 function fib(n::Integer)::Integer
-    if n == 0
-        return 0
-    else
-        if n == 1
-            return 1
-        else
-            return fib(n-1) + fib(n-2)
-        end
+    if n == 0 return 0
     end
+    if n == 1 return 1
+    end
+    return fib(n-1) + fib(n-2)
 end
 
 function gcd_recursive(a::Integer, b::Integer)::Integer
-    if b == 0
-        return a
-    else
-        return gcd_recursive(b, a % b)
+    if b == 0 return a
     end
+    return gcd_recursive(b, a % b)
 end
 
 function main()
-
     a::Integer = 20/3
     b::Float64 = 20/3
     println(a)
