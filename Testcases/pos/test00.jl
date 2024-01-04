70e021280c2ed211a381bbe5bc5468f0327bfe24
Silas Kraume <silas.kraume1552@gmail.com>
2024-01-04 01:16:23 +0100

    changed test semantics

--- Testcases/pos/test00.jl
+++ Testcases/pos/test00.jl
@@ -7,17 +7,19 @@ function main()
     a::Integer = 1
         b::Integer = 1
         temp::Integer = 0
+    println("Fibonacci:")
         while a < 144
             temp = b
                 b = a + b
                 a = temp
                 println(a)
         end
-    println(lol("a"))
+    println("Calling other functions:")
+    println(test_function("a"))
     x()
 end
 
-function lol(x::String)::Float64
+function test_function(x::String)::Float64
     a::Float64 = 20/3
     a = 5
     if (5/5+5*2 > 2)
