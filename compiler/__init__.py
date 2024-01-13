c5216c041b5a52b82ec9b7ae5496c19fda182bd4
Silas Kraume <silas.kraume1552@gmail.com>
2024-01-13 16:25:52 +0100

    bump version

--- compiler/__init__.py
+++ compiler/__init__.py
@@ -1,7 +1,7 @@
 """A Julia to Jasmin compiler using ANTLR in Python as defined in the CoBa-Project."""
 
 __project__ = 'CoBa_Projekt'
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 __sysversion__ = '3.10.11 (tags/v3.10.11:7d4cc5a, Apr  5 2023, 00:38:17) [MSC v.1929 64 bit (AMD64)]'
 __author__ = 'Silas A. Kraume'
 __url__ = 'https://github.com/SilenZcience/CoBa_Projekt'
